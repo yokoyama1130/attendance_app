f82d705b18a08a0005fedffeab9e5edb17dfe26e
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-04-11 18:28:33 +0900

    now

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -18,7 +18,6 @@ class AttendanceController extends Controller
     {
         $attendance = Attendance::whereDate('date', Carbon::today())->first();
         $action = $request->input('action');
-        $now = Carbon::now('Asia/Tokyo');
 
         if (!$attendance) {
             if ($action === 'clock_in') {
