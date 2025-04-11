147853fbfb2f4eaf7218563f0424bc86ec7ae774
yokoyama1130 <ryogotan@ezweb.ne.jp>
2025-04-11 15:59:40 +0900

    Atendance_app_migrated

---
+++ database/migrations/2025_04_11_061348_create_attendances_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->timestamp('clock_in')->nullable();
+            $table->timestamp('break_start')->nullable();
+            $table->timestamp('break_end')->nullable();
+            $table->timestamp('clock_out')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
